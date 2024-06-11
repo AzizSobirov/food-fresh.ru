8f16c25f56d1f842f2650faac6ca0dd70185b157
Aziz Sobirov <azizsobirovhazarasp@gmail.com>
2024-06-11 19:30:38 +0500

    11.06.2024

--- imports.d.ts
+++ imports.d.ts
@@ -1,5 +1,11 @@
+import TheAside from "./src/components/TheAside.astro";
+import TheCatalog from "./src/components/TheCatalog.astro";
+import TheCategories from "./src/components/TheCategories.astro";
 import TheFooter from "./src/components/TheFooter.astro";
 import TheHeader from "./src/components/TheHeader.astro";
+import TheIntro from "./src/components/TheIntro.astro";
+import TheOffers from "./src/components/TheOffers.astro";
 import ThePopup from "./src/components/ThePopup.astro";
+import TheTop from "./src/components/TheTop.astro";
 
-export { TheFooter, TheHeader, ThePopup };
+export { TheAside, TheCatalog, TheCategories, TheFooter, TheHeader, TheIntro, TheOffers, ThePopup, TheTop };
--- public/assets/js/app.js
+++ public/assets/js/app.js
@@ -1,5 +1,29 @@
 // TODO: Header
 const header = document.querySelector(".header");
 window.addEventListener("scroll", function () {
-  header.classList.toggle("header-sticky", window.scrollY > 60);
+  header.classList.toggle("header-sticky", window.scrollY > 0);
 });
+
+// TODO: Swipers
+var introSwiper = new Swiper(".intro .swiper", {
+  slidesPerView: 1,
+  effect: 'fade',
+  loop: true,
+  autoplay: {
+    delay: 3000,
+  },
+  pagination: {
+    el: ".intro .swiper-pagination",
+    clickable: true,
+  }
+});
+
+var catalogSwiper = new Swiper(".catalog .swiper", {
+  slidesPerView: 6,
+  spaceBetween: 25,
+  navigation: {
+    nextEl: ".catalog .btn-next",
+    prevEl: ".catalog .btn-prev",
+  }
+});
+

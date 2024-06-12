4caf22345f9a74456ed3c983410ad0dc71f2c962
Aziz Sobirov <azizsobirovhazarasp@gmail.com>
2024-06-12 21:01:02 +0500

    12.06.2024

--- imports.d.ts
+++ imports.d.ts
@@ -4,7 +4,6 @@ import TheCategories from "./src/components/TheCategories.astro";
 import TheClients from "./src/components/TheClients.astro";
 import TheFooter from "./src/components/TheFooter.astro";
 import TheHeader from "./src/components/TheHeader.astro";
-import TheIntro from "./src/components/TheIntro.astro";
 import TheLeaders from "./src/components/TheLeaders.astro";
 import TheOffers from "./src/components/TheOffers.astro";
 import ThePopup from "./src/components/ThePopup.astro";
@@ -12,4 +11,4 @@ import TheSklad from "./src/components/TheSklad.astro";
 import TheSpecial from "./src/components/TheSpecial.astro";
 import TheTop from "./src/components/TheTop.astro";
 
-export { TheAside, TheCatalog, TheCategories, TheClients, TheFooter, TheHeader, TheIntro, TheLeaders, TheOffers, ThePopup, TheSklad, TheSpecial, TheTop };
+export { TheAside, TheCatalog, TheCategories, TheClients, TheFooter, TheHeader, TheLeaders, TheOffers, ThePopup, TheSklad, TheSpecial, TheTop };

f2a4d5bf4dc3881dc17db81c34b72fcf1aeb40fa
Aziz Sobirov <azizsobirovhazarasp@gmail.com>
2024-06-13 23:05:56 +0500

    13.06.2024

--- public/assets/js/app.js
+++ public/assets/js/app.js
@@ -4,6 +4,30 @@ window.addEventListener("scroll", function () {
   header.classList.toggle("header-sticky", window.scrollY > 0);
 });
 
+
+//** buy moscow **/
+let buy = document.querySelector(".buy");
+if (buy) {
+  const dropwown = buy.querySelector(".buy__dropdown");
+  const content = buy.querySelector(".buy__dropdown-content");
+  const btn = buy.querySelector(".btn");
+
+  btn.addEventListener("click", () => {
+    const isActive = dropwown.classList.contains("active");
+
+    if (!isActive) {
+      dropwown.classList.add("active");
+      dropwown.style.maxHeight = content.scrollHeight + "px";
+      btn.innerHTML = "Скрыть";
+    } else {
+      dropwown.classList.remove("active");
+      dropwown.style.maxHeight = "110px";
+      btn.innerHTML = "Подробнее";
+    }
+  });
+}
+
+
 // TODO: Swipers
 var introSwiper = new Swiper(".intro .swiper", {
   slidesPerView: 1,
@@ -44,3 +68,15 @@ var clientsSwiper = new Swiper(".clients .swiper", {
     prevEl: ".clients .btn-prev",
   }
 });
+
+var categoryLeaders = new Swiper(".category__leaders .swiper", {
+  slidesPerView: 4,
+  spaceBetween: 25,
+  navigation: {
+    nextEl: ".category__leaders .btn-next",
+    prevEl: ".category__leaders .btn-prev",
+  }
+});
+
+
+

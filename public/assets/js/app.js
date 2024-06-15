0b36a1798e95402f2ed2fd5c73847043a2fab0fe
Aziz Sobirov <azizsobirovhazarasp@gmail.com>
2024-06-15 23:47:36 +0500

    15.06.2024

--- public/assets/js/app.js
+++ public/assets/js/app.js
@@ -26,6 +26,87 @@ if (buy) {
   });
 }
 
+
+// TODO: Popup
+[].forEach.call(document.querySelectorAll(".v-mask"), function (input) {
+  let keyCode;
+  function mask(event) {
+    event.keyCode && (keyCode = event.keyCode);
+    let pos = this.selectionStart;
+    if (pos < 3) event.preventDefault();
+    let matrix = "+7 (___) ___-__-__",
+      i = 0,
+      def = matrix.replace(/\D/g, ""),
+      val = this.value.replace(/\D/g, ""),
+      newValue = matrix.replace(/[_\d]/g, function (a) {
+        return i < val.length ? val.charAt(i++) || def.charAt(i) : a;
+      });
+    i = newValue.indexOf("_");
+    if (i != -1) {
+      i < 5 && (i = 3);
+      newValue = newValue.slice(0, i);
+    }
+    let reg = matrix
+      .substr(0, this.value.length)
+      .replace(/_+/g, function (a) {
+        return "\\d{1," + a.length + "}";
+      })
+      .replace(/[+()]/g, "\\$&");
+    reg = new RegExp("^" + reg + "$");
+    if (
+      !reg.test(this.value) ||
+      this.value.length < 5 ||
+      (keyCode > 47 && keyCode < 58)
+    )
+      this.value = newValue;
+    if (event.type == "blur" && this.value.length < 5) this.value = "";
+
+    if (this.value.length == 18 || this.value.length == 0) {
+      input.dataset.numbervalid = "true";
+    } else {
+      input.dataset.numbervalid = "false";
+    }
+  }
+
+  input.addEventListener("input", mask, false);
+  input.addEventListener("focus", mask, false);
+  input.addEventListener("blur", mask, false);
+  input.addEventListener("keydown", mask, false);
+});
+
+const popup = document.querySelector(".popup");
+function openPopup() {
+  popup.style.display = 'flex'
+}
+function closePopup() {
+  popup.style.display = 'none'
+}
+
+const popupForm = document.querySelector(".popup form");
+popupForm?.addEventListener("submit", (e) => {
+  e.preventDefault();
+
+  let name = popupForm.querySelector("#name");
+  let email = popupForm.querySelector("#email");
+  let phone = popupForm.querySelector("#phone");
+
+  if (phone.dataset.numbervalid === "true") {
+    // alert("Спасибо за заявку. В ближайшее время с вами свяжутся.");
+    let content = document.querySelector(".popup #form-content");
+    let success = document.querySelector(".popup #form-success");
+
+    content.style.display = "none";
+    success.style.display = "flex";
+
+    setTimeout(() => {
+      closePopup();
+      content.style.display = "flex";
+      success.style.display = "none";
+    }, 3000);
+  }
+});
+
+
 // TODO: Swipers
 var introSwiper = new Swiper(".intro .swiper", {
   slidesPerView: 1,
@@ -130,10 +211,20 @@ var clientsSwiper = new Swiper(".clients .swiper", {
 });
 
 var categoryLeaders = new Swiper(".category__leaders .swiper", {
-  slidesPerView: 4,
-  spaceBetween: 25,
+  slidesPerView: 'auto',
+  spaceBetween: 10,
   navigation: {
     nextEl: ".category__leaders .btn-next",
     prevEl: ".category__leaders .btn-prev",
+  },
+  breakpoints: {
+    641: {
+      slidesPerView: 3,
+      spaceBetween: 20,
+    },
+    1381: {
+      slidesPerView: 4,
+      spaceBetween: 25,
+    }
   }
 });
